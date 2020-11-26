d35112a49f9c320d8441e170e361c389d569ddde
PamelaBarboza <pamelarabarboza@gmail.com>
2020-11-26 01:20:30 -0300

    Formulario de login

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import Login from "./componentes/Login";
+import { BrowserRouter as Router } from "react-router-dom";
+import FormLogin from "./componentes/FormLogin";
+
+
 const App = () => {
   return (
-    <div>
-      <Login />
-    </div>
-  )
+    <Router>
+      <div container>
+        <Login />
+        <FormLogin />
+      </div>
+    </Router>
+  );
 }
 
 export default App
--- my-app/src/componentes/Login.jsx
+++ my-app/src/componentes/Login.jsx
@@ -3,11 +3,14 @@ import portada from '../img/portada.png'
 import logo from '../img/aes-gener.jpg'
 import "./Login.css";
 
+
+
 const Login = () => {
     return (
       <div className="login">
         <img className="imgPortada" src={portada} alt="imagenPortada" />
         <img className="logo" src={logo} alt="logoPortada" />
+       
       </div>
     );
 }
